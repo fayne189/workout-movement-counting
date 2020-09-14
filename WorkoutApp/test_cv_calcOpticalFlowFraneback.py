39d0042071302b6a807062dc7e5fb589e076aaa4
neng_189 <42114362+neng5201314@users.noreply.github.com>
2020-09-14 09:42:47 +0900

    Update test_cv_calcOpticalFlowFraneback.py

--- WorkoutApp/test_cv_calcOpticalFlowFraneback.py
+++ WorkoutApp/test_cv_calcOpticalFlowFraneback.py
@@ -10,29 +10,65 @@ if __name__ == '__main__':
     cam = cv2.VideoCapture(0)
     ret, prev = cam.read()
     prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
-    flow = None
+    mask = np.zeros_like(prev)
+    mask[..., 1] = 255
+    flow = []
+
+    filename = 'out.mp4'
+    height, width, channels = prev.shape
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(filename, fourcc, 20, (width, height))
+
     while True:
         ret, img = cam.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # 使用Gunnar Farneback算法计算密集光流
-        flow = cv2.calcOpticalFlowFarneback(prevgray, gray, flow, 0.5, 3, 50, 3, 5, 1.2, 0)
+        if len(flow) == 0:
+            flow = cv2.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 50, 3, 7, 1.1, 0)
+        else:
+            flow = cv2.calcOpticalFlowFarneback(prevgray, gray, flow, 0.5, 3, 50, 3, 7, 1.1, 0)
+
+        # Computes the magnitude and angle of the 2D vectors
+        magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+
+        # Sets image hue according to the optical flow direction
+        mask[..., 0] = angle * 180 / np.pi / 2
+
+        # Sets image value according to the optical flow magnitude (normalized)
+        mask[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+
+        # Converts HSV to RGB (BGR) color representation
+        rgb = cv2.cvtColor(mask, cv2.COLOR_HSV2BGR)
+        # print(flow)
         prevgray = gray
-        # 绘制线
-        h, w = gray.shape[:2]
-        y, x = np.mgrid[step / 2:h:step, step / 2:w:step].reshape(2, -1).astype(int)
-        fx, fy = flow[y, x].T
-        lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
-        lines = np.int32(lines)
+        # # 绘制线
+        # h, w = gray.shape[:2]
+        # y, x = np.mgrid[step / 2:h:step, step / 2:w:step].reshape(2, -1).astype(int)
+        # # print(y, x)
+        # fx, fy = flow[y, x].T
+        # lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
+        # lines = np.int32(lines)
+        # print(lines)
+        # color = ()
+        # line = []
+        # for l in lines:
+        #     if l[0][0]-l[1][0]>3 or l[0][1]-l[1][1]>3:
+        #         line.append(l)
+        #     if (l[0][1] - l[1][1]) > 0:
+        #         color = (0,0,255)
+        #     else:
+        #         color = (255,0,0)
 
-        line = []
-        for l in lines:
-            if l[0][0]-l[1][0]>3 or l[0][1]-l[1][1]>3:
-                line.append(l)
+        # cv2.polylines(img, line, 0, color)
+        cv2.imshow('rgb', rgb)
+        cv2.imshow('mask', mask)
+        # save video 
+        # out.write(rgb)
+        # cv2.imshow('flow', flow)
 
-        cv2.polylines(img, line, 0, (0,255,255))
-        cv2.imshow('flow', img)
-
-        ch = cv2.waitKey(5)
+        ch = cv2.waitKey(1)
         if ch == 27:
             break
+    cam.release()
+    out.release()
     cv2.destroyAllWindows()
